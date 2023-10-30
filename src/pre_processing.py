d52f89bce11881059975aae038843ebca012087a
mw <79407675+mwdevhub@users.noreply.github.com>
2023-10-30 12:22:08 +0100

    Update pre_processing.py changing zoom function and new import function

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 
 #Regarding reviewer 1 comment: Random Seed
 # ToDo: ADD RANDOM SEED
-# random.seed(10)
+random.seed(14)
         
+
 def import_input_data_gray(log, directory='01_input_images'):
     input_data = []
     class_count = {}
@@ -77,11 +78,8 @@ def split_input_data(log, input_data, training=70, validation=15, testing=15):
     print(f'ALL SAMPLES {sum([len(i) for i in input_data])}')
 
     for sample_class in list(input_data):
-
         sample_class_copy = sample_class.copy()
-
         random.shuffle(sample_class_copy)
-
         train = int(len(sample_class_copy) * (training / 100))
         validate = int(len(sample_class_copy) * (validation / 100))
         test = int(len(sample_class_copy) * (testing / 100))
@@ -109,8 +107,8 @@ def prepare_images(log, dataset, cropping_x=300, cropping_y=0, resolution=128):
 
     for sample in dataset:
         image = sample[0]
-
         image = image[cropping_x:image.shape[0] - cropping_x, cropping_y:image.shape[1] - cropping_y]
+        #show_output_images(image, 'CROPPED1')
         image = cv2.resize(image, (resolution, resolution))
         sample[0] = image
 
@@ -128,12 +126,25 @@ def rotateImage(image, angle):
 
 #Regarding Reviewer 1 comment on GradCam and the Model learning wing size
 #ToDo: write function to augment images via zoom
-#def zoomImage(image, zoom):
-#    return cv2.resize(image, None, fx=zoom_factor, fy=zoom_factor)
+def zoomImage(image, zoom_factor, resolution):
+    
+    # Enlarge images
+    image = cv2.resize(image, None, fx=zoom_factor, fy=zoom_factor)
+    #show_output_images(image, 'ZOOM')
+    # Get new image center
+    image_center = image.shape[0] // 2
+    # Get lower and upper boundary to crop image back to final resolution
+    lower = image_center - (resolution // 2)
+    upper = image_center + (resolution // 2)
+    # Apply the cropping to the image
+    image = image[lower:upper, lower:upper]   #cv2.resize(image, (resolution, resolution))
+    #show_output_images(image, 'ZOOMED FINAL RES')
+    #print(image.shape)
+
+    return image
     
 
-
-def data_augmentation(log, dataset, num_of_augmentations=10, max_shift=20, max_rotation=15):
+def data_augmentation(log, dataset, num_of_augmentations=10, max_shift=20, max_rotation=15,  resolution=128):
     print(f'TOTAL NUMBER OF TRAINING SAMPLES BEFORE AUGMENTATION: {len(dataset)}')
     augmented_dataset = []
     for sample in dataset:
@@ -142,7 +153,11 @@ def data_augmentation(log, dataset, num_of_augmentations=10, max_shift=20, max_r
             M = np.float32([[1, 0, random.randint(max_shift * -1, max_shift)], [0, 1, random.randint(max_shift * -1, max_shift)]])
             aug_image = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
             aug_image = rotateImage(aug_image, random.uniform(max_rotation * -1, max_rotation))
-            #aug_image = zoomImage(aug_image, random.uniform(0.5, 2))
+            
+            # Here I suggest to only enlarge the images and crop them to the final resolution.
+            # Zoom without changing the image dimensions.
+            # The CNN only works with images of the same dimensions.
+            aug_image = zoomImage(aug_image, random.uniform(1.0, 1.5), resolution)
             augmented_dataset.append([aug_image, sample[1], sample[2], sample[3][:-4] + f' Aug {i}' + sample[3][-4:]])
     augmented_dataset += dataset
     print(f'TOTAL NUMBER OF TRAINING SAMPLES AFTER AUGMENTATION: {len(augmented_dataset)}')
@@ -185,6 +200,59 @@ def save_output_image(log, image, name):
     heatmap = image
     heatmap = np.uint8(255 * heatmap)
     heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-
     path = os.path.join(log['OUTPUT_PATH'], name)
     cv2.imwrite(path, heatmap)
+
+
+
+def import_input_data_new_rgb(log, directory='01_input_images', folder='train'):
+    input_data = []
+    class_count = {}
+    hitmatrix = np.eye(len(log['CLASS_LABELS']))
+    num_samples = []
+    smallest_sample_num = 0
+
+    dir = os.path.join(directory, folder)
+
+    print(dir)
+
+    if folder == 'train':
+        for label_index, label in enumerate(log['CLASS_LABELS']):
+            path = os.path.join(dir, label)
+            num_samples.append(len(os.listdir(path)))
+            num_samples.sort()
+            smallest_sample_num = num_samples[0]
+
+        for label_index, label in enumerate(log['CLASS_LABELS']):
+            path = os.path.join(dir, label)
+            sample_class = []
+            class_count[label] = 0
+            for image_index in range(smallest_sample_num):  # range(20))
+                image_name = os.listdir(path)[image_index]
+                image_path = os.path.join(path, image_name)
+                image = cv2.imread(image_path)
+                sample_class.append([image, hitmatrix[label_index], label, image_name])
+                class_count[label] += 1
+                input_data.append([image, hitmatrix[label_index], label, image_name])
+                
+    else:
+        
+        for label_index, label in enumerate(log['CLASS_LABELS']):
+            path = os.path.join(dir, label)
+            sample_class = []
+            class_count[label] = 0
+            for image_index in range(len(os.listdir(path))):  # range(20))
+                image_name = os.listdir(path)[image_index]
+                image_path = os.path.join(path, image_name)
+                image = cv2.imread(image_path)
+                sample_class.append([image, hitmatrix[label_index], label, image_name])
+                class_count[label] += 1
+                input_data.append([image, hitmatrix[label_index], label, image_name])
+                
+    random.shuffle(input_data)
+    print(f'FOLDER {folder}: ', class_count)
+    print('\n')
+    return input_data
+
+
+
